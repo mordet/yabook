9a6908ca4f57ffca7a5fe186988227796cfea72c
Dmitrii Raspopov <mordeth@yandex-team.ru>
2019-09-06 15:20:35 +0300

    cc telegram: simple message parsing

--- telegram/src/main.rs
+++ telegram/src/main.rs
@@ -5,6 +5,38 @@ use telegram_bot::*;
 
 static TOKEN_ENV: &str = "TELEGRAM_BOT_TOKEN";
 
+fn substr(data: &String, offset: i64, length: i64) -> String {
+  data.chars()
+      .skip(offset as usize)
+      .take(length as usize)
+      .collect()
+}
+
+async fn process_text_message(
+    api: &Api, data: &String, entities: &Vec<MessageEntity>, message: &Message) {
+    if let Some(login) = message.from.username.as_ref() {
+        println!("<{}>: {}", login, data);
+    }
+
+    for entity in entities {
+        let value = substr(&data, entity.offset, entity.length);
+        match entity.kind {
+            MessageEntityKind::Mention => {
+                println!("You mentioned {}", value)
+            },
+            MessageEntityKind::BotCommand => println!("Bot command found: {}", value),
+            _ => {}
+        }
+    }
+
+    match api.send(message.text_reply(
+        format!("Привет, {}! Команда echo дала '{}'",
+                &message.from.first_name, data))).await {
+        Ok(_) => {},
+        Err(err) => println!("Не удалось отправить сообщение получателю: {:?}", err),
+    }
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Error> {
     let telegram_token = env::var(TOKEN_ENV)
@@ -19,13 +51,8 @@ async fn main() -> Result<(), Error> {
         let update = update?;
 
         if let UpdateKind::Message(message) = update.kind {
-            if let MessageKind::Text {ref data, ..} = message.kind {
-                println!("<{}>: {}", &message.from.first_name, data);
-
-                api.send(message.text_reply(
-                    format!("Привет, {}! Команда echo дала '{}'",
-                    &message.from.first_name, data)
-                )).await?;
+            if let MessageKind::Text {ref data, ref entities} = message.kind {
+                process_text_message(&api, &data, &entities, &message).await;
             }
         }
     }
